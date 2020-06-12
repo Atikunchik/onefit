17877926b570b28c52a3270decd8a95c6d80c611
Albert Zhumashev <albert.zhumashev@gmail.com>
2020-06-12 10:37:42 +0600

    psycorg2 binary added

--- TASK1/settings.py
+++ TASK1/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'API',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +75,12 @@ WSGI_APPLICATION = 'TASK1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dbreviews',
+        'USER': 'atikunchik',
+        'PASSWORD': 'Atikun00',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
